e2105b5cfdf998c19c191db51c73ddcf2dc1024d
Himanish Mandrekar <mandrekarh25@gmail.com>
2021-12-24 01:17:30 +0530

    implement one time view

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,23 +2,30 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sprout/pages/firstPage.dart';
 import 'package:sprout/pages/get_started.dart';
+import 'package:after_layout/after_layout.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Loading extends StatefulWidget {
   @override
-  _LoadingState createState() => _LoadingState();
+  LoadingState createState() => LoadingState();
 }
+class LoadingState extends State<Loading> with AfterLayoutMixin<Loading>  {
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
 
-class _LoadingState extends State<Loading> 
-{
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        const Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => FirstPage())));
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => FirstPage()));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => GetStarted()));
+    }
   }
 
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
