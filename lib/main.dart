9407d591d15f679234ab6815f1f4cdc53bcf984a
Himanish Mandrekar <mandrekarh25@gmail.com>
2021-12-24 00:11:58 +0530

    add Plant disease Scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sprout/pages/firstPage.dart';
+import 'package:sprout/pages/homePage.dart';
 
-void main() {
+List<CameraDescription>? cameras;
+
+void main() async {
+  //Initialize cameras for plant disease scanner
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+
+  //Initialize notification service
   AwesomeNotifications().initialize(
     'resource://drawable/res_notification_logo',
     [
@@ -26,24 +35,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      //////////////////// THEMING ////////////////////
-      
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          color: Colors.transparent,
-          toolbarTextStyle: TextStyle()),
-        fontFamily: 'Inter',
+        //////////////////// THEMING ////////////////////
 
-      ),
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              color: Colors.transparent, toolbarTextStyle: TextStyle()),
+          fontFamily: 'Inter',
+        ),
 
-      /////////////////////////////////////////////////
-      home: 
-      
-      ScreenUtilInit(designSize: const Size(414,896), builder: () => 
-      
-      FirstPage()
-      
-      )
-    );
+        /////////////////////////////////////////////////
+        home: ScreenUtilInit(
+            designSize: const Size(414, 896), builder: () => HomePage()));
   }
 }
---
+++ lib/pages/ML_plant_scanner.dart
@@ -0,0 +1,98 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import '../main.dart';
+import 'package:tflite/tflite.dart';
+
+class PlantScanner extends StatefulWidget {
+  @override
+  _PlantScannerState createState() => _PlantScannerState();
+}
+
+class _PlantScannerState extends State<PlantScanner> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCamera();
+    loadmodel();
+  }
+
+  loadCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((ImageStream) {
+            cameraImage = ImageStream;
+            runModel();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+      // ignore: avoid_function_literals_in_foreach_calls
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+        model: "assets/ML_Model/model.tflite",
+        labels: "assets/ML_Model/labels.txt");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width,
+            child: !cameraController!.value.isInitialized
+                ? Container()
+                : AspectRatio(
+                    aspectRatio: cameraController!.value.aspectRatio,
+                    child: CameraPreview(cameraController!),
+                  ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            output,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
